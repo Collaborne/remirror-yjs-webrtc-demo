d9958181bfb0a3a0136426f6bb24366e5795e31c
whawker <whawker@users.noreply.github.com>
2021-06-10 15:09:21 +0000

    Deploying to gh-pages from  @ 5c295e26aa021455c83c674019f71735a60f8db7 🚀

--- src/hooks/useCurrentUser.ts
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react';
-import UserProvider, { User } from '../UserProvider';
-
-function useCurrentUser(): User {
-	return useContext(UserProvider);
-}
-
-export default useCurrentUser;
